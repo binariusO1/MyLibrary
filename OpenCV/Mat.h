dfbc07694bed097ce9c6e85df00cbcd89d37cacb
binariusO1 <ryszard1191@gmail.com>
2020-09-12 23:26:08 +0200

    add OpenCV library

---
+++ OpenCV/Mat.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <opencv2/core.hpp>
+
+/// <summary>
+/// Extension for Mat class
+/// </summary>
+
+namespace ocv {
+	class Mat : private cv::Mat
+	{
+	public:
+
+	};
+}
+
---
+++ OpenCV/alghorithm.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+namespace ocv {
+	class algorithms
+	{
+
+	};
+}
+
